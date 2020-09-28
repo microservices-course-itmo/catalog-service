67e1f3be85283227dd95b07ef93a059f8c9f9c6e
santatostada <asabinayulia@gmail.com>
2020-09-28 16:15:56 +0300

    adding swagger annotations for controllers

--- src/main/java/com/wine/to/up/catalog/service/controller/DiscountInfoController.java
+++ src/main/java/com/wine/to/up/catalog/service/controller/DiscountInfoController.java
@@ -1,6 +1,8 @@
 package com.wine.to.up.catalog.service.controller;
 
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
@@ -11,16 +13,29 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/discountinfo")
 @Validated
 @Slf4j
+@Api(value = "DiscountInfoController", description = "Discount info controller")
 public class DiscountInfoController {
+
+    @ApiOperation(value = "Create discount info",
+            nickname = "createDiscountInfo", notes = "",
+            tags={ "discount-info-controller", })
     @PostMapping("/")
     public void createDiscountInfo(){
 
     }
 
+
+    @ApiOperation(value = "Get discount info by id",
+            nickname = "getDiscountInfoById", notes = "",
+            tags={ "discount-info-controller", })
     @GetMapping("/{id}")
     public void getDiscountInfoById(){
     }
 
+
+    @ApiOperation(value = "Update discount info by id",
+            nickname = "updateDiscountInfoById", notes = "",
+            tags={ "discount-info-controller", })
     @PutMapping("/{id}")
     public void updateDiscountInfoById(){
 
