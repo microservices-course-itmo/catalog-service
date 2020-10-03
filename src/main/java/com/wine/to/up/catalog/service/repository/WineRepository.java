e39ae6f026bb176203b08eb56cc98ca5367b123e
sipov <sipovsky2010@yandex.ru>
2020-10-04 01:42:27 +0300

    small fix

--- src/main/java/com/wine/to/up/catalog/service/repository/WineRepository.java
+++ src/main/java/com/wine/to/up/catalog/service/repository/WineRepository.java
@@ -1,7 +1,6 @@
 package com.wine.to.up.catalog.service.repository;
 
 import com.wine.to.up.catalog.service.domain.entities.Wine;
-import com.wine.to.up.catalog.service.domain.entities.*;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
