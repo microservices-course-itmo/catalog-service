c4c2d906de7da17b497d9a317d2a3d51f1fbeff7
YaPravaATyNet <amdedovich@yandex.ru>
2020-10-09 21:07:43 +0300

    Create new version of database tables

--- src/main/resources/db/migration/V1.0.1__init_wine_table.sql
+++ src/main/resources/db/migration/V1.0.1__init_wine_table.sql
@@ -1,85 +1,85 @@
-CREATE TYPE Color AS ENUM ('red', 'white', 'rose');
-CREATE TYPE Sugar AS ENUM ('dry', 'medium dry', 'medium', 'sweet');
+CREATE TYPE Color AS ENUM('red', 'white', 'rose');
+CREATE TYPE Sugar AS ENUM('dry', 'medium dry', 'medium', 'sweet');
 
-CREATE TABLE Brands
+CREATE TABLE grape
 (
-    brandID   serial PRIMARY KEY,
-    brandName TEXT
+    id   UUID PRIMARY KEY,
+    name VARCHAR(255)
 );
 
-CREATE TABLE Countries
+CREATE TABLE region
 (
-    countryID   serial PRIMARY KEY,
-    countryName TEXT
+    id      UUID PRIMARY KEY,
+    name    VARCHAR(255),
+    country VARCHAR(255)
+);
+
+CREATE TABLE brand
+(
+    id   UUID PRIMARY KEY,
+    name VARCHAR(255),
+);
+
+CREATE TABLE producer
+(
+    id   UUID PRIMARY KEY,
+    name VARCHAR(255),
 );
 
 CREATE TABLE Wine
 (
-    wineID      VARCHAR(255) PRIMARY KEY,
-    picture     BYTEA        NOT NULL,
-    brandID     INT,
-    countryID   INT          NOT NULL,
-    volume      real         NOT NULL,
-    abv         real         NOT NULL,
-    color       Color        NOT NULL,
-    sugar       Sugar        NOT NULL,
-    name        varchar(255) NOT NULL,
-    year        varchar(10)  NOT NULL,
-    grapes_info varchar(255),
-
-    FOREIGN KEY (brandID)
-        REFERENCES Brands (brandID),
-    FOREIGN KEY (countryID)
-        REFERENCES Countries (countryID)
+    id          UUID PRIMARY KEY,
+    name        VARCHAR(255),
+    producer_id UUID,
+    brand_id    UUID,
+    region_id   UUID,
+    grape_id    UUID,
+    avg         REAL,
+    color       Color,
+    sugar       Sugar,
+    year        INT,
+    FOREIGN KEY (producer_id)
+        REFERENCES producer (id),
+    FOREIGN KEY (brand_id)
+        REFERENCES brand (id),
+    FOREIGN KEY (region_id)
+        REFERENCES region (id),
+    FOREIGN KEY (grape_id)
+        REFERENCES grape (id)
 );
 
-CREATE TABLE PositionPrice
+CREATE TABLE shop
 (
-    priceID serial PRIMARY KEY,
-    wineID  varchar(255),
-    shopID  int,
-    value   real
+    id   UUID PRIMARY KEY,
+    site VARCHAR(255),
 );
 
-CREATE TABLE Discount
+CREATE TABLE wine_position
 (
-    wineID varchar(255),
-    shopID int,
-    value  real
+    id           UUID PRIMARY KEY,
+    wine_id      UUID,
+    shop_id      UUID,
+    price        REAL,
+    actual_price REAL,
+    link_to_wine VARCHAR(255),
+    volume       REAL,
+    description  TEXT,
+    gastronomy   TEXT,
+    image        BYTEA,
+    FOREIGN KEY (wine_id)
+        REFERENCES Wine (id),
+    FOREIGN KEY (shop_id)
+        REFERENCES shop (id)
 );
 
-CREATE TABLE Grapes
+CREATE TABLE review
 (
-    grapeID   VARCHAR(255) PRIMARY KEY,
-    grapeName VARCHAR(255),
-    grapeCode VARCHAR(255)
-);
-
-CREATE TABLE WineGrapesInfo
-(
-    id      serial PRIMARY KEY,
-    wineID  VARCHAR(255),
-    grapeID VARCHAR(255),
-    FOREIGN KEY (wineID)
-        REFERENCES Wine (wineID),
-    FOREIGN KEY (grapeID)
-        REFERENCES Grapes (grapeID)
-);
-
-CREATE TABLE Shops
-(
-    shopID   serial PRIMARY KEY,
-    shopName TEXT
-);
-
-CREATE TABLE Catalog
-(
-    winePositionID serial PRIMARY KEY,
-    wineID         VARCHAR(255),
-    shopID         INT,
-    price          INT,
-    FOREIGN KEY (wineID)
-        REFERENCES Wine (wineID),
-    FOREIGN KEY (shopID)
-        REFERENCES Shops (shopID)
+    wine_id      UUID,
+    user_id      UUID,
+    user_name    VARCHAR(255),
+    bisness_name VARCHAR(255),
+    text         TEXT,
+    mark         REAL,
+    FOREIGN KEY (wine_id)
+        REFERENCES Wine (id)
 );
