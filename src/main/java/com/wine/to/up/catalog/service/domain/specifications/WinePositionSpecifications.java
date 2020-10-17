59c07efe2088c245bc10bda5794dfd367e9bf5de
sipov <sipovsky2010@yandex.ru>
2020-10-17 21:31:36 +0300

    changed to lambdas

--- src/main/java/com/wine/to/up/catalog/service/domain/specifications/WinePositionSpecifications.java
+++ src/main/java/com/wine/to/up/catalog/service/domain/specifications/WinePositionSpecifications.java
@@ -3,46 +3,21 @@ package com.wine.to.up.catalog.service.domain.specifications;
 import com.wine.to.up.catalog.service.domain.entities.WinePosition;
 import org.springframework.data.jpa.domain.Specification;
 
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
-
 public class WinePositionSpecifications {
 
     public static Specification<WinePosition> winePositionHasWebsiteLink() {
-        return new Specification<WinePosition>() {
-            @Override
-            public Predicate toPredicate(Root<WinePosition> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
-                return criteriaBuilder.notEqual(root.get("linkToWine"), "");
-            }
-        };
+        return (Specification<WinePosition>) (root, query, criteriaBuilder) -> criteriaBuilder.notEqual(root.get("linkToWine"), "");
     }
 
     public static Specification<WinePosition> winePositionHasImage() {
-        return new Specification<WinePosition>() {
-            @Override
-            public Predicate toPredicate(Root<WinePosition> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
-                return criteriaBuilder.notEqual(root.get("image"), null);
-            }
-        };
+        return (Specification<WinePosition>) (root, query, criteriaBuilder) -> criteriaBuilder.notEqual(root.get("image"), null);
     }
 
     public static Specification<WinePosition> winePositionVolumeIsLessThan(float requestVolume){
-        return new Specification<WinePosition>() {
-            @Override
-            public Predicate toPredicate(Root<WinePosition> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
-                return criteriaBuilder.lessThan(root.get("volume"), requestVolume);
-            }
-        };
+        return (Specification<WinePosition>) (root, query, criteriaBuilder) -> criteriaBuilder.lessThan(root.get("volume"), requestVolume);
     }
 
     public static Specification<WinePosition> winePositionVolumeIsGreaterThan(float requestVolume){
-        return new Specification<WinePosition>() {
-            @Override
-            public Predicate toPredicate(Root<WinePosition> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
-                return criteriaBuilder.greaterThan(root.get("volume"), requestVolume);
-            }
-        };
+        return (Specification<WinePosition>) (root, query, criteriaBuilder) -> criteriaBuilder.greaterThan(root.get("volume"), requestVolume);
     }
 }
