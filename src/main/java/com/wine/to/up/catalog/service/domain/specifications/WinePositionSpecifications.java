23a2e7a1ce1a4ee5308ba2d4dc0067d23854839a
sipov <sipovsky2010@yandex.ru>
2020-10-18 00:37:55 +0300

    added actual_price specs

--- src/main/java/com/wine/to/up/catalog/service/domain/specifications/WinePositionSpecifications.java
+++ src/main/java/com/wine/to/up/catalog/service/domain/specifications/WinePositionSpecifications.java
@@ -20,4 +20,12 @@ public class WinePositionSpecifications {
     public static Specification<WinePosition> winePositionVolumeIsGreaterThan(float requestVolume){
         return (Specification<WinePosition>) (root, query, criteriaBuilder) -> criteriaBuilder.greaterThan(root.get("volume"), requestVolume);
     }
+
+    public static Specification<WinePosition> winePositionActualPriceLessThan(float requestActualPrice){
+        return (Specification<WinePosition>) (root, query, criteriaBuilder) -> criteriaBuilder.lessThan(root.get("actual_price"), requestActualPrice);
+    }
+
+    public static Specification<WinePosition> winePositionActualPriceGreaterThan(float requestActualPrice){
+        return (Specification<WinePosition>) (root, query, criteriaBuilder) -> criteriaBuilder.greaterThan(root.get("actual_price"), requestActualPrice);
+    }
 }
