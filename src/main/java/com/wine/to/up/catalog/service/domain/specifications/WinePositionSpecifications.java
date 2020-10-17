766f12a73a0bf84f20fcc97c44b49692bed42d1a
sipov <sipovsky2010@yandex.ru>
2020-10-17 21:00:59 +0300

    Added simple WinePositionSpecifications

---
+++ src/main/java/com/wine/to/up/catalog/service/domain/specifications/WinePositionSpecifications.java
@@ -0,0 +1,39 @@
+package com.wine.to.up.catalog.service.domain.specifications;
+
+import com.wine.to.up.catalog.service.domain.entities.WinePosition;
+import org.springframework.data.jpa.domain.Specification;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+public class WinePositionSpecifications {
+
+    public static Specification<WinePosition> winePositionHasWebsiteLink() {
+        return new Specification<WinePosition>() {
+            @Override
+            public Predicate toPredicate(Root<WinePosition> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
+                return criteriaBuilder.notEqual(root.get("linkToWine"), "");
+            }
+        };
+    }
+
+    public static Specification<WinePosition> winePositionHasImage() {
+        return new Specification<WinePosition>() {
+            @Override
+            public Predicate toPredicate(Root<WinePosition> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
+                return criteriaBuilder.notEqual(root.get("image"), null);
+            }
+        };
+    }
+
+    public static Specification<WinePosition> winePositionVolumeIsLessThan(float requestVolume){
+        return new Specification<WinePosition>() {
+            @Override
+            public Predicate toPredicate(Root<WinePosition> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
+                return criteriaBuilder.lessThan(root.get("volume"), requestVolume);
+            }
+        };
+    }
+}
