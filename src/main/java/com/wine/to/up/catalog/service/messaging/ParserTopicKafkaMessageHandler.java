35931bea7c181de490a537aa031574898b4facd6
YRaphael <raphael.yakupov.99@gmail.com>
2020-11-22 16:19:20 +0300

    check jenkins

--- src/main/java/com/wine/to/up/catalog/service/messaging/ParserTopicKafkaMessageHandler.java
+++ src/main/java/com/wine/to/up/catalog/service/messaging/ParserTopicKafkaMessageHandler.java
@@ -9,8 +9,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.annotation.Propagation;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -397,3 +397,4 @@ public class ParserTopicKafkaMessageHandler implements KafkaMessageHandler<WineP
         return bySugarName;
     }
 }
+
