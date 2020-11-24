5885b7189eee15d8e4096955d67855829309cb10
YRaphael <raphael.yakupov.99@gmail.com>
2020-11-25 02:47:14 +0300

    fix migration

--- src/main/resources/db/migration/V1.2.7__migrate_wine_positions_of_amwine_with_different_shop_to_one_shop.sql
+++ src/main/resources/db/migration/V1.2.7__migrate_wine_positions_of_amwine_with_different_shop_to_one_shop.sql
@@ -1,16 +1,16 @@
 update wine_position
 set shop_id = 'f8a2114c-2c6c-44d5-9e1b-fb1b5e226e1f'
-from wine_position
+from wine_position as wp
 inner join shop as s
-    on wine_position.shop_id = s.id
+    on wp.shop_id = s.id
 where s.site = 'amwine.com';
 
 INSERT INTO shop VALUES ('48db3cfe-b615-44a4-a327-c891dd334267', 'simplewine.ru');
 update wine_position
 set shop_id = '48db3cfe-b615-44a4-a327-c891dd334267'
-from wine_position
+from wine_position as wp
          inner join shop as s
-                    on wine_position.shop_id = s.id
+                    on wp.shop_id = s.id
 where s.site = '';
 
 delete from shop
